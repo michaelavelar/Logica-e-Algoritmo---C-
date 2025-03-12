7d773d27a92bcbf66738c34fcc36b2452579d4ef
Michael Avelar <michael.avelarn@gmail.com>
2025-03-12 13:33:40 -0300

    02-variaveis/michael

--- 01conceito/Program.cs
+++ 01conceito/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+// Comentario na mesma linha
+/*
+    Comentario em
+    mais de uma
+    linha
+*/
+
 Console.WriteLine("Hello, World!");
+
+
+Console.Write("Ola, Mundo!");
+Console.Write("Ola, Mundo!");
---
+++ 02variaveis/Program.cs
@@ -0,0 +1,20 @@
+﻿// Textos
+string nome = "Michael";
+
+// Letras 
+char inicial = 'r';
+
+// Numeros inteiros
+int idade = 32;
+
+// Numero reais
+double peso = 91.2;
+
+// Logica
+bool estuda = true;
+Console.WriteLine(nome);
+Console.WriteLine(inicial);
+Console.WriteLine(idade);
+Console.WriteLine(peso);
+Console.WriteLine(estuda);
+
