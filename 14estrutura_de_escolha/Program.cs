fd0df760a4b0cd01bf4745689db42b4b40a319be
Michael Avelar <michael.avelarn@gmail.com>
2025-03-12 16:19:53 -0300

    estrutura-de-escolhas/michael

---
+++ 14estrutura_de_escolha/Program.cs
@@ -0,0 +1,20 @@
+﻿// Variavel
+string linguagem = "C#";
+
+
+switch (linguagem)
+{
+    case "HTML":
+        Console.WriteLine("Linguagem de marcacao");
+        break;
+    case "CSS":
+        Console.WriteLine("Linguagem de estilos");
+        break;
+    case "C#":
+        Console.WriteLine("Linguagem de programacao");
+        break;
+
+    default:
+        Console.WriteLine("Linguagem de desconhecida");
+        break;
+}
---
+++ 15switch_expression/Program.cs
@@ -0,0 +1,33 @@
+﻿// // Variavel
+string linguagem = "C#";
+
+
+// switch (linguagem)
+// {
+//     case "HTML":
+//         Console.WriteLine("Linguagem de marcacao");
+//         break;
+//     case "CSS":
+//         Console.WriteLine("Linguagem de estilos");
+//         break;
+//     case "C#":
+//         Console.WriteLine("Linguagem de programacao");
+//         break;
+
+//     default:
+//         Console.WriteLine("Linguagem de desconhecida");
+//         break;
+// }
+
+
+// Expressao switch
+
+string retorno = linguagem switch
+{
+    "HTML" => "LINGAGEM DE MARCACAO",
+    "CSS" => "LINGAGEM DE ESTILOS",
+    "C#" => "LINGAGEM DE PROGRAMACAO",
+    _ => "LINGUAGEM DESCONHECIDA"
+
+};
+Console.WriteLine(retorno);
