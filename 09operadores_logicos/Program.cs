f5cc929794338bd857236008058616d973fbaa84
Michael Avelar <michael.avelarn@gmail.com>
2025-03-12 15:31:19 -0300

    aula09e10/michael

---
+++ 09operadores_logicos/Program.cs
@@ -0,0 +1,14 @@
+﻿// Variaveis
+
+double projeto1 = 7;
+double projeto2 = 5;
+
+
+// Operador logico E()  
+Console.WriteLine(projeto1 >= 7 && projeto2 >= 7);
+
+// Operador OU(|)"
+Console.WriteLine(projeto1 >= 7 || projeto2 >= 7);
+
+// Operador de NEGACAO
+Console.WriteLine(!false);
---
+++ 10condicionais_simples/Program.cs
@@ -0,0 +1,13 @@
+﻿// Variavel
+int idade = 16;
+
+// Condicional
+
+if (idade >= 18)
+{
+    Console.WriteLine("Pode Obter a CNH");
+}
+else
+{
+    Console.WriteLine("Nao pode obter a CNH");
+}
