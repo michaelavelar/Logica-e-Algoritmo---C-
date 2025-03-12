2c6cb9ebf29c262493ccedf268e66df270b0deaf
Michael Avelar <michael.avelarn@gmail.com>
2025-03-12 15:03:32 -0300

    operadores/michael

---
+++ 07operadores_aritmeticos/Program.cs
@@ -0,0 +1,27 @@
+﻿// Variaveis
+
+int numero1 = 30;
+int numero2 = 2;
+
+// Divisao
+Console.WriteLine(numero1 / numero2);
+
+// Multiplicacao
+Console.WriteLine(numero1 * numero2);
+
+// Subtracao
+Console.WriteLine(numero1 - numero2);
+
+// Adicao
+Console.WriteLine(numero1 + numero2); ; ;
+
+// Modulo ou Restante da divisao % => Exemplo dividir 3/10 o restante da divisao é 1 
+Console.WriteLine(numero1 % numero2);
+
+// Incrementador = aumentar mais 1 ao valor
+numero1++;
+Console.WriteLine(numero1);
+
+// Decrementador = diminuir menos / ao valor
+numero1--;
+Console.WriteLine(numero1);
