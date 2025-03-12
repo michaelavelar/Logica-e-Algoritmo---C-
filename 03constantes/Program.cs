59a5528327ce3678f57e717bed98c3f1b5c6b107
Michael Avelar <michael.avelarn@gmail.com>
2025-03-12 13:44:03 -0300

    03-constantes/michael

---
+++ 03constantes/Program.cs
@@ -0,0 +1,18 @@
+﻿// Criando variavei
+
+string nome = "Michael";
+
+// ALeterar valor da variavel
+nome = "Gabriela";
+
+// Exibir nome da variavel
+Console.WriteLine(nome);
+
+
+// Criando uma constante
+
+const string nome2 = "Michael";
+
+Console.WriteLine(nome2);
+
+
