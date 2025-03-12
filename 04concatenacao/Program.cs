4571acf040daa89370984e9c7f7b5f37a8e340bb
Michael Avelar <michael.avelarn@gmail.com>
2025-03-12 14:05:54 -0300

    04-concatenacao/michael

---
+++ 04concatenacao/Program.cs
@@ -0,0 +1,14 @@
+﻿// constantes
+
+const string nome = "Michael";
+const string sobrenome = "Avelar";
+const int idade = 32;
+const string cidade = "São Paulo";
+
+// concatenacao através do +
+
+Console.WriteLine("Nome: " + nome + ", Sobrenome: " + sobrenome + ", Idade: " + idade + ", Cidade: " + cidade);
+
+// concatenacao atraves de interpolacao de strings
+
+Console.WriteLine($"{nome} mora em {cidade}.");
