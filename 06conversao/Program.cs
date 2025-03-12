7df0734f1bb81a8eda428711b78061aec1d7a9ea
Michael Avelar <michael.avelarn@gmail.com>
2025-03-12 14:32:48 -0300

    06-conversao/michael

---
+++ 06conversao/Program.cs
@@ -0,0 +1,10 @@
+﻿// Obter primeiro numero
+Console.WriteLine("Informe um numero");
+int numero1 = Convert.ToInt16(Console.ReadLine());
+
+// Obter segundo numero
+Console.WriteLine("Informe outro numero");
+int numero2 = Convert.ToInt16(Console.ReadLine());
+
+// Realizar a soma
+Console.WriteLine($"A soma é: {numero1 + numero2}");
