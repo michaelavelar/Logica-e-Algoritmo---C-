e8686b1dafe3b59c5c0a40fcf08e6c073abe5023
Michael Avelar <michael.avelarn@gmail.com>
2025-03-13 10:21:58 -0300

    aula 29 e 30

--- 29list/Program.cs
+++ 29list/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// List
+List<string> nomes = new List<string>();
+
+// Adicionar nomes
+nomes.Add("Michael");
+nomes.Add("Gabriela");
+nomes.Add("Thor");
+
+// Laço de repetição
+for (int indice = 0; indice < nomes.Count; indice++)
+{
+    Console.WriteLine(nomes[indice]);
+}
