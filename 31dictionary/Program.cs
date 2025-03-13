9213b63d536a8f00beaca8e07e74d775dca7e526
Michael Avelar <michael.avelarn@gmail.com>
2025-03-13 10:31:05 -0300

    aula 31 32

--- 31dictionary/Program.cs
+++ 31dictionary/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Criando um Dictionary para armazenar os nomes
+Dictionary<int, string> nomes = new Dictionary<int, string>();
+
+// Adicionando elementos no Dictionary
+nomes.Add(1, "Michael");
+nomes.Add(2, "Gabriela");
+nomes.Add(3, "Thor");
+
+// Exibir um elemento através da chave:
+Console.WriteLine($"O elemento da chave 1 é: {nomes[1]}");
+
+// Exibindo os elementos do Dictionary
+Console.WriteLine("Listagem de todos os elementos do Dictionary:");
+foreach (var item in nomes)
+{
+    Console.WriteLine($"{item.Key} - {item.Value}");
+}
+
