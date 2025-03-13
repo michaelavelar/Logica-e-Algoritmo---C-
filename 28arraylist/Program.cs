2db2138226597e3dafd6bbc29dceb31b87161bf5
Michael Avelar <michael.avelarn@gmail.com>
2025-03-13 10:01:49 -0300

    aula28/michael

--- 28arraylist/Program.cs
+++ 28arraylist/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Collections;
+
+//ArrayList
+ArrayList lista = new ArrayList();
+
+//cadastrar dados
+lista.Add("Michael");
+lista.Add(32);
+lista.Add(true);
+
+//listar
+foreach (var l in lista)
+{
+    Console.WriteLine(l);
+}
