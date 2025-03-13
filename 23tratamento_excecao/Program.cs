3b22fd63e5b7e9c3b518ff7e64b2dc13c575ed5d
Michael Avelar <michael.avelarn@gmail.com>
2025-03-13 09:32:33 -0300

    feat:aula 22 e 23

---
+++ 23tratamento_excecao/Program.cs
@@ -0,0 +1,16 @@
+﻿// Tratamento de excecao com try catch e Finally
+
+try
+{
+    Console.WriteLine("Informe um numero");
+    int numero = Convert.ToInt16(Console.ReadLine());
+}
+catch (Exception error)
+{
+    Console.WriteLine("Houve uma falha:" + error.Message);
+}
+finally
+{
+    Console.WriteLine("Sistema finalizado");
+}
+
