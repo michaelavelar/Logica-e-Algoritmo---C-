9f8db61cf71e64287988bbaec86c5df301ff7ab9
Michael Avelar <michael.avelarn@gmail.com>
2025-03-13 15:13:25 -0300

    add

--- 11condicionais_encadeadas/obj/Debug/net8.0/11condicionais_encadeadas.AssemblyInfo.cs
+++ 11condicionais_encadeadas/obj/Debug/net8.0/11condicionais_encadeadas.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("11condicionais_encadeadas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63d4a3863ef2e26c0cdc43f1b57e90be5a17daab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+050878bf941c0cfbd821d5c02c200a0f5395f047")]
 [assembly: System.Reflection.AssemblyProductAttribute("11condicionais_encadeadas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("11condicionais_encadeadas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
