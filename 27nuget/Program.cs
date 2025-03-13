62e19ae9ac805aafb8072e946961865050b57272
Michael Avelar <michael.avelarn@gmail.com>
2025-03-13 09:53:44 -0300

    aula27/michael

--- 27nuget/Program.cs
+++ 27nuget/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Gerenciador de pacotes - complementos para o projeto é encontrado no NuGet
+
+using PdfSharpCore.Drawing;
+using PdfSharpCore.Fonts;
+using PdfSharpCore.Pdf;
+using PdfSharpCore.Utils;
+
+GlobalFontSettings.FontResolver = new FontResolver();
+
+var document = new PdfDocument();
+var page = document.AddPage();
+
+var gfx = XGraphics.FromPdfPage(page);
+var font = new XFont("Arial", 20, XFontStyle.Bold);
+
+var textColor = XBrushes.Black;
+var layout = new XRect(20, 20, page.Width, page.Height);
+var format = XStringFormats.Center;
+
+gfx.DrawString("Hello World!", font, textColor, layout, format);
+
+document.Save("helloworld.pdf");
