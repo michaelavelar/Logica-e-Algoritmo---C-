f58fe1de94159de816a611ec7ffd7358da1a1467
Your Name <you@example.com>
2025-03-15 14:53:33 -0300

    poo aula 35 e 35 /  michael

---
+++ 35poointroducao/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// Cronograma
+// Veja abaixo o a primeira parte do nosso cronograma, dividido em 17 aulas:
+
+// Introdução ao paradigma orientado a objeto
+// Classe
+// Atributo
+// Método
+// Objeto
+// Modificadores de acesso
+// Encapsulamento
+// Construtor
+// Herança
+// Polimorfismo
+// Abstração
+// Interface
+// Atributos e métodos estáticos
---
+++ 36pooconceitos/Program.cs
@@ -0,0 +1,28 @@
+﻿// 2 - Conceitos
+// Olá pessoal, essa é a segunda aula do treinamento de orientação a objetos com C#.
+
+// Vamos conhecer os principais conceitos da o
+// rientação a objetos.
+
+// O que é orientação a objetos
+// Programação Orientada a Objetos ou simplesmente POO, é um paradigma de programação. Os paradigmas são maneiras para estruturarmos nossos projetos, conheça alguns:
+
+// Funcional
+// Lógico
+// Declarativo
+// Iperativo
+// Orientado a eventos
+// O conceito da programação orientada a objetos teve início na década de 60 por: Ole-Johan Dahl e Kristen Nygaard. A primeira linguagem que teve suporte ao paradigma foi a Simula.
+
+// Características
+// Conheça as principais características do paradigma:
+
+// Reutilização de código
+// Fácil manutenção
+// Encapsulamento
+// Pilares
+// Esse paradigma é dividido em três pilares muito importantes:
+
+// Abstração: Que consiste em identificar as características de um objeto ou entidade ao mundo real.
+// Encapsulamento: Esconder detalhes internos de um objeto.
+// Herança: Permite herdas características e comportamente de outras classes
